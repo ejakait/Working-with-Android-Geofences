4a448f5bd60dda36bfb1284cb98f167734554ddf
ejakait <ejakaitcaleb@gmail.com>
2017-06-14 01:29:13 +0300

    notifications commit

--- app/src/main/java/com/andresdavid/testgeofencinggideoracle/GeofencingService.java
+++ app/src/main/java/com/andresdavid/testgeofencinggideoracle/GeofencingService.java
@@ -8,17 +8,17 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
 import java.util.List;
-
-/**
- * Created by andresdavid on 13/09/16.
- */
+//creating the intent service
 public class GeofencingService extends IntentService {
+
     protected static final String LOG_TAG = GeofencingService.class.getName();
 
+    //constructor
     public GeofencingService() {
         super(LOG_TAG);
     }
 
+    //handling the event(entering or exiting the geofence
     @Override
     protected void onHandleIntent(Intent intent) {
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
@@ -32,9 +32,11 @@ public class GeofencingService extends IntentService {
             Geofence geofence = geofenceList.get(0);
             String requestId = geofence.getRequestId();
             if(transition == Geofence.GEOFENCE_TRANSITION_ENTER){
+                EnteringGeofence.notify(this,"You are withing the radius of the Geofence" ,2 );
                 Log.d(LOG_TAG,String.format("GEOFENCE_TRANSITION_ENTER on %s",requestId));
             }
             else if(transition == Geofence.GEOFENCE_TRANSITION_EXIT){
+                ExitingGeofence.notify(this, "You are our of the radius of the Geofence",2);
                 Log.d(LOG_TAG,String.format("GEOFENCE_TRANSITION_EXIT  on %s",requestId));
             }
         }
--- app/src/main/java/com/andresdavid/testgeofencinggideoracle/MainActivity.java
+++ app/src/main/java/com/andresdavid/testgeofencinggideoracle/MainActivity.java
@@ -4,11 +4,13 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -28,7 +30,10 @@ import com.google.android.gms.location.LocationServices;
 
 import static com.google.android.gms.location.LocationServices.API;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     protected static final String LOG_TAG = MainActivity.class.getName();
     private static final String GEOFENCE_ID = "myGeofenceID";
@@ -73,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 stopGeofenceMonitor();
             }
         });
-        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1234);
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},1234);
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -128,6 +133,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     .setInterval(10000)
                     .setFastestInterval(5000)
                     .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
 
         } catch (Exception e) {
@@ -156,6 +171,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         if (!googleApiClient.isConnected()) {
             Log.d(LOG_TAG, "GoogleApiClient is not Conectec");
         } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             LocationServices.GeofencingApi.addGeofences(googleApiClient, geofencingRequest, pendingIntent)
                     .setResultCallback(new ResultCallback<Status>() {
                         @Override
